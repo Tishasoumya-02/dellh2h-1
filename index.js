017d9dad1e41ae95333c85745370c708ec6b5a22
Tishasoumya-02 <tishasoumya@gmail.com>
2021-11-25 00:11:25 +0530

    Added template for Bot

---
+++ index.js
@@ -0,0 +1,29 @@
+const express=require('express')
+require('./server/db/connect')
+const path = require("path");
+const bodyParser = require("body-parser");
+const config = require("./server/config/keys");
+const app=express();
+const port=process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api/dialogflow', require('./server/routes/dialogflow'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+
+app.listen(port,()=>{
+    console.log('Connection to server made')
+})
--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-require('./db/connect')
-
-const app=express();
-const port=process.env.PORT || 3000;
-
-app.listen(port,()=>{
-    console.log('Connection to server made')
-})
