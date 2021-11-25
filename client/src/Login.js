c49828e5fef9079545b129d093322e76c1584250
Bhavya Singh <64300122+BhavyaSingh7@users.noreply.github.com>
2021-11-25 20:44:39 +0530

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,18 +1,23 @@
 import React from "react";
 
 function login(){
-    return (<div>
+    return (<div class="login-main">
+        <div class="lm">
          <div  style={{
-             height: 400, width: 450, 
-             border:'3px solid black', borderRadius: '7px'
-             }}>
+             height: 300, width: 350, 
+             border:'5px solid black', borderRadius: '7px'
+             }} class="login-box">
 
-             <form class="" action=" " method="">
-                 <label>Please Enter The Key</label><br/>
-                 <input type = "number" name=" " value="" />
-                 <input type="submit" name=""/>
+             <form onSubmit={MoveToChat}>
+                 <label class="login-text" for="login">   <b>Please Enter The Key</b>    </label><br/>
+                 <input type = "number" name="login " value="login" />
+                 <input type="submit" value="submit"/>
              </form>
+
+             <img src="https://cdn.dribbble.com/users/634508/screenshots/5058273/media/81e8b38e8346fb7402ee98525ee4ab5a.gif" 
+              alt="not found" class="login-img"/>
          </div> 
+         </div>
     </div>
     )
 }
